931bece0a8791470f7b8b95ca68d98f6b4c08431
sriindah24 <62129413+sriindah24@users.noreply.github.com>
2020-03-27 06:57:55 +0700

    Tugas Laravel

---
+++ app_mahasiswa/resources/views/admin/tescontent.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.tes')
+@section('content')
+
+<div class="panel">
+       <div class="panel-body">
+                <h3>Daftar Barang</h3>
+                <br></br>
+                 <table class="table bordered">
+                     <tr>
+                         <td width='300'><h4>Biodata</h4></td>
+                         <td width='300'><h4>Studi Kasus</h4></td>
+                     </tr>
+                     <tr>
+                         <td>Nama   : Ni Wayan Sri Indah Yani</td>
+                         <td>Judul  : Sistem Manajemen Toko Kelontong</td>
+                     </tr>
+                     <tr rowspan='2'>
+                         <td>NIM   : 1815051094</td>
+                         <td>Penjelasan: Sistem Manajemen Toko Kelontong merupakan sebuah gagasan untuk membuat sebuah manajemen toko baik itu manajemen dalam hal penjualan,pembelian maupun barang pada suatu toko kelontong</td>
+                     </tr>
+                     <tr>
+                         <td>Program Studi: Pendidikan Teknik Informatika</td>
+                         <td></td>
+                     </tr>
+
+                 </table>
+                 
+
+
+</div>
+</div>
+@endsection
